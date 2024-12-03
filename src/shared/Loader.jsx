0081d6e8b53a3f5d6466ab674d7d5d1536c1cfd6
Shubh Arya <shubh.arya37@gmail.com>
2024-12-03 22:26:43 +0530

    added text as prop

--- src/shared/Loader.jsx
+++ src/shared/Loader.jsx
@@ -1,9 +1,9 @@
-const Loader = () => {
+const Loader = ({ text }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="text-center">
         <div className="loader border-t-4 border-blue-500 border-solid rounded-full w-16 h-16 animate-spin mx-auto"></div>
-        <p className="mt-4 text-white text-lg font-semibold">Preparing Interview...</p>
+        <p className="mt-4 text-white text-lg font-semibold">{text}</p>
       </div>
     </div>
   );
