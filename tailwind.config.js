35f7a6e6f889ccbcf510e20ce4cd0fc677ca6023
Shubh Arya <shubh.arya37@gmail.com>
2024-12-17 10:02:13 +0530

    default theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#1D4ed8',
+        'primary-foreground': '#ffffff', // Replace with your desired color (e.g., Tailwind's indigo-500)
+        background: '#1D4ed8', // Replace with your desired background color
+        ring: '#93C5FD' // Add additional colors as needed
+      }
+    }
   },
   plugins: []
 };
