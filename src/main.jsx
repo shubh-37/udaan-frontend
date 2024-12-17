63492e5b0db19419a34a8b785a65c752d5ebc207
Shubh Arya <shubh.arya37@gmail.com>
2024-12-17 09:59:16 +0530

    wrapped context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import AuthProvider from './context/AuthContextProvider.jsx';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </StrictMode>
 );
