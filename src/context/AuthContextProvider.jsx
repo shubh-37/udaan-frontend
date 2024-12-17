df06a2c5be227f2d47d0daf492bce2aa38217aee
Shubh Arya <shubh.arya37@gmail.com>
2024-12-17 09:54:57 +0530

    added auth context

---
+++ src/context/AuthContextProvider.jsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { createContext } from 'react';
+
+export const authContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+export default function AuthProvider({ children }) {
+  async function signUpUser(user) {
+    try {
+      const response = await axios.post('https://udaan-backend.ip-dynamic.org/signup', user, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      if (response.status === 200) {
+        if (response.data.message) {
+          localStorage.setItem('token', response.data.message);
+          localStorage.setItem('username', response.data.username);
+          return 'success';
+        }
+      }
+    } catch (error) {
+      if (error.response.status === 500) {
+        return 'failure';
+      } else if (error.response.status === 400) {
+        return 'duplicate';
+      }
+    }
+  }
+
+  async function loginUser(user) {
+    try {
+      const response = await axios.post('https://udaan-backend.ip-dynamic.org/login', user);
+      console.log(response);
+      if (response.status === 200) {
+        if (response.data.message) {
+          localStorage.setItem('token', response.data.message);
+          localStorage.setItem('username', response.data.username);
+          return 'success';
+        }
+      }
+    } catch (error) {
+      if (error.response.status === 500) {
+        return 'failure';
+      }
+    }
+  }
+
+  return <authContext.Provider value={{ signUpUser, loginUser }}>{children}</authContext.Provider>;
+}
