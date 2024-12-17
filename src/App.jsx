46116626720a3e369af1aa8a35940ca2029b8119
Shubh Arya <shubh.arya37@gmail.com>
2024-12-17 09:59:02 +0530

    added new routes and auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Interview from './pages/Interview';
 import InterviewReview from './pages/Review';
+import SignupForm from './pages/Signup';
+import LoginForm from './pages/Login';
+import RequiresAuth from './shared/RequiresAuth';
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/interview" element={<Interview />} />
-        <Route path="/review" element={<InterviewReview />} />
+        <Route
+          path="/interview"
+          element={
+            <RequiresAuth>
+              <Interview />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/review"
+          element={
+            <RequiresAuth>
+              <InterviewReview />
+            </RequiresAuth>
+          }
+        />
+        <Route path="/sign-up" element={<SignupForm />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </div>
   );
