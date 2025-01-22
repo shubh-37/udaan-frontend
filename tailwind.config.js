79d639c843173faeef3904f5e31a8fecd34a5e2f
Shubh Arya <shubh.arya37@gmail.com>
2025-01-22 19:39:55 +0530

    added marquee styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: ['class'],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        primary: '#1D4ed8',
-        'primary-foreground': '#ffffff', // Replace with your desired color (e.g., Tailwind's indigo-500)
-        background: '#1D4ed8', // Replace with your desired background color
-        ring: '#93C5FD' // Add additional colors as needed
-      }
-    }
+  	extend: {
+  		animation: {
+  			marquee: 'marquee var(--duration) infinite linear',
+  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
+  		},
+  		keyframes: {
+  			marquee: {
+  				'0%': {
+  					transform: 'translateX(100%)'
+  				},
+  				'100%': {
+  					transform: 'translateX(-100%)'
+  				},
+  				from: {
+  					transform: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'marquee-vertical': {
+  				from: {
+  					transform: 'translateY(0)'
+  				},
+  				to: {
+  					transform: 'translateY(calc(-100% - var(--gap)))'
+  				}
+  			}
+  		},
+  		colors: {
+  			primary: '#1D4ed8',
+  			'primary-foreground': '#ffffff',
+  			background: '#1D4ed8',
+  			ring: '#93C5FD'
+  		}
+  	}
   },
   plugins: []
 };
