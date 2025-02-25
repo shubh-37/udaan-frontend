c6c41ab47e9a3de258da18741873a3907b7b1109
Mohit Rana <mohitrana@Mohits-MacBook-Air.local>
2025-02-25 12:58:56 +0530

    Added shadcn components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-red-500 text-white shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background3 shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
