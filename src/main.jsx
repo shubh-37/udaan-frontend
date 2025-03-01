acd84734e9b06372adbfaa3bb0c965482d3e57e6
Shubh Arya <shubh.arya37@gmail.com>
2025-03-02 04:06:20 +0530

    added aptitude context

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AuthProvider from './context/AuthContextProvider.jsx';
 import './index.css';
 import App from './App.jsx';
-import { ThemeProvider } from "./components/theme-provider"
+import { ThemeProvider } from './components/theme-provider';
+import AptitudeProvider from './context/AptitudeContextProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <AuthProvider>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <App />
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <AptitudeProvider>
+            <App />
+          </AptitudeProvider>
         </ThemeProvider>
       </AuthProvider>
     </Router>
